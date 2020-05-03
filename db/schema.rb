df6afee6a13273fcf026af976dc946cf13421463
leoggonzalez <leoggonzalez@hotmail.com>
2020-05-03 18:23:12 +0200

    added users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_103721) do
+ActiveRecord::Schema.define(version: 2020_05_03_161924) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2020_04_26_103721) do
     t.datetime "inactive_on"
     t.string "recurrent"
     t.index ["account_id"], name: "index_entries_on_account_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
