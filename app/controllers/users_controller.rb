6d98d137d29af5874686033c49ecda77b05eb91f
leoggonzalez <leoggonzalez@hotmail.com>
2020-05-19 22:52:02 +0200

    edit user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "User was created successfully"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User was updated succesfully"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :accounts
   resources :entries
+  resources :users, except: [:new]
 
   root 'home#index'
+
+  get 'signup', to: 'users#new'
 end
